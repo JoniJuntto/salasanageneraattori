ca14d63d7ba3a883efc6cc1bdd14d7b9927d340a
JoniJuntto <bgm043@myy.haaga-helia.fi>
2021-12-11 17:43:18 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,133 @@
 import logo from './logo.svg';
 import './App.css';
+import { Stack, TextField, Typography, Button, Checkbox } from '@mui/material';
+import { useState } from 'react';
+import { numbers, lowerCaseLetters, upperCaseLetters, specialCharacters } from './characters';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
 
 function App() {
+
+  const [password, setPassword] = useState('');
+  const [passwordL, setPasswordL] = useState(15);
+  const [haveNumbers, setHaveNumbers] = useState(false);
+  const [havelowerCaseLetters, setHaveLowerCaseLetters] = useState(false);
+  const [haveUpperCaseLetters, setHaveUpperCaseLetters] = useState(false);
+  const [haveSpecials, setHaveSpecials] = useState(false);
+
+  const notify = (notif, state) => {
+    if(state === 'error'){
+    toast.error(notif, {
+      position: "top-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      });
+    }else if(state === 'success'){
+      toast.success(notif, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        });
+    }
+  }
+
+
+
+  const handleLengthChange = e => {
+    setPasswordL(e.target.value);
+  }
+
+  const generatePassword = (e) => {
+    if (!haveNumbers && !haveSpecials && !haveUpperCaseLetters && !havelowerCaseLetters) {
+      notify("One value must be selected", 'error');
+      return;
+    }
+    let passwordCharacters = '';
+
+    if (havelowerCaseLetters) {
+      passwordCharacters = passwordCharacters + lowerCaseLetters
+    }
+    if (haveUpperCaseLetters) {
+      passwordCharacters = passwordCharacters + upperCaseLetters
+    }
+    if (haveSpecials) {
+      passwordCharacters = passwordCharacters + specialCharacters
+    }
+    if (haveNumbers) {
+      passwordCharacters = passwordCharacters + numbers
+    }
+    setPassword(createPasswordFromCharacters(passwordCharacters));
+    notify('Password generated', 'success')
+
+  }
+  const createPasswordFromCharacters = (characters) => {
+    let password = '';
+    const charactersL = characters.length
+
+    for (var i = 0; i < passwordL; i++) {
+      const characterAt = Math.round(Math.random() * charactersL);
+      password = password + characters.charAt(characterAt);
+    }
+    return password
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Stack>
+        <Typography variant='h4'>Include lower case letters</Typography>
+        <Checkbox
+          checked={havelowerCaseLetters}
+          onChange={((e) => setHaveLowerCaseLetters(e.target.checked))}
+          inputProps={{ 'aria-label': 'controlled' }}
+        />
+        <Typography variant='h4'>Include Upper case letters</Typography>
+        <Checkbox
+          checked={haveUpperCaseLetters}
+          onChange={((e) => setHaveUpperCaseLetters(e.target.checked))}
+          inputProps={{ 'aria-label': 'controlled' }}
+        />
+        <Typography variant='h4'>include special characters</Typography>
+        <Checkbox
+          checked={haveSpecials}
+          onChange={((e) => setHaveSpecials(e.target.checked))}
+          inputProps={{ 'aria-label': 'controlled' }}
+        />
+        <Typography variant='h4'>include numbers</Typography>
+        <Checkbox
+          checked={haveNumbers}
+          onChange={((e) => setHaveNumbers(e.target.checked))}
+          inputProps={{ 'aria-label': 'controlled' }}
+        />
+        <TextField
+          value={passwordL}
+          type="number"
+          variant='outlined'
+          onChange={handleLengthChange}
+        />
+        <Button onClick={generatePassword} >Generate password</Button>
+        <Typography variant='h4'>Generated password: </Typography>
+        <Typography variant='h1'>{password}</Typography>
+      </Stack>
+      <ToastContainer 
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover/>
     </div>
   );
 }
---
+++ src/characters.js
@@ -0,0 +1,4 @@
+export const numbers = '0123456789'
+export const upperCaseLetters = 'ABCDEFGHJIKLMNOPQRSTUVWXYZÅÄÖ'
+export const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyzåäö'
+export const specialCharacters = '!^+%&/()=?_#$½§{[]}|;:>÷`<.*-'
